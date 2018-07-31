b2c3de87b44ca9f67824858c115b90fea411f87d
nestor salinas <nestorasm88@gmail.com>
2018-07-31 07:19:21 -0500

    chore

---
+++ inventory.rb
@@ -0,0 +1,20 @@
+
+
+def electronics(price, name)
+  if name == 'iphone'
+    puts "#{price}"
+  elsif name == 'ipad'
+    puts "#{price}"
+  elsif name == 'mac'
+    puts "#{price} "
+  else
+    puts 'I am sorry I don\'t have that in my inventory!'
+  end
+end
+
+electronics(80, 'ipad')
+electronics(160, 'iphone')
+electronics(2300, 'mac')
+electronics(2300, 'drone')
+
+
