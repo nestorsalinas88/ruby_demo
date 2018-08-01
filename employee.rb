811f11d8c9cea160634c0970699df400c468d6c2
nestor salinas <nestorasm88@gmail.com>
2018-08-01 09:42:55 -0500

    chore:refactored employee class to practice inheritance

--- employee.rb
+++ employee.rb
@@ -24,10 +24,10 @@ class Employee
 
 
 
-    puts "Name: #{@name}"
+    puts "Name: #{name}"
 
     # when diving make sure to use at least one FLOAT
-    pay_period = (@salary / 365.0) * 14
+    pay_period = (salary / 365.0) * 14
     # format sequence to remove extra decimal places
     pay_period = format("%0.2f", pay_period)
     puts "pay for this period was $#{pay_period}"
@@ -35,19 +35,15 @@ class Employee
 end
 
 # parameters in initializing method
-dorian = Employee.new("this guy", 40000)
-dorian.print_stub
+Employee.new("this guy", 40000).print_stub
 
 
-sandra = Employee.new("cole brown", 40000)
-sandra.print_stub
+Employee.new("cole brown", 40000).print_stub
 
 
-empty = Employee.new("amy",50000)
-empty.print_stub
+Employee.new("amy",50000).print_stub
 
-this = Employee.new("This other dude", 34000.00)
-this.print_stub
+Employee.new("This other dude", 34000.00).print_stub
 
 class Car
   def initialize(engine)
@@ -85,3 +81,85 @@ end
 # object = MyClass.new
 # puts "my object #{object}"
 # object.first_method
+
+
+# refctoring Employee Class
+
+class Employee
+  attr_reader :name
+  def name=(name)
+    if name == ""
+      raise "Name can't be blank!"
+    end
+    @name = name
+  end
+  def initialize(name = "Anonymous")
+    self.name = name
+  end
+  def print_name
+    puts "Name: #{name}"
+  end
+
+end
+
+class SalariedEmployee < Employee
+
+  attr_reader :salary
+  def salary=(salary)
+    if salary < 0
+      raise "A salary of #{salary} isn't valid!"
+    end
+    @salary = salary
+  end
+  def initialize(name = "Anonymous", salary = 0.0)
+    super(name)
+    self.salary = salary
+  end
+  def print_pay_stub
+    print_name
+    pay_for_period = (salary / 365.0) * 14
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{formatted_pay}"
+  end
+end
+class HourlyEmployee < Employee
+  def self.security_guard(name)
+    HourlyEmployee.new(name, 19.25, 30)
+  end
+  def self.cashier(name)
+    HourlyEmployee.new(name, 12.75, 25)
+  end
+  def self.janitor(name)
+    HourlyEmployee.new(name, 10.50, 20)
+  end
+  attr_reader :hourly_wage, :hours_per_week
+  def hourly_wage=(hourly_wage)
+    if hourly_wage < 0
+      raise "An hourly wage of #{hourly_wage} isn't valid!"
+    end
+    @hourly_wage = hourly_wage
+  end
+  def hours_per_week=(hours_per_week)
+    if hours_per_week < 0
+      raise "#{hours_per_week} hours per week isn't valid!"
+    end
+    @hours_per_week = hours_per_week
+  end
+  def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+    super(name)
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
+  end
+  def print_pay_stub
+    print_name
+    pay_for_period = hourly_wage * hours_per_week * 2
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{formatted_pay}"
+  end
+end
+jane = SalariedEmployee.new("Jane Doe", 50000)
+jane.print_pay_stub
+angela = HourlyEmployee.security_guard("Angela Matthews")
+ivan = HourlyEmployee.cashier("Ivan Stokes")
+angela.print_pay_stub
+ivan.print_pay_stub
