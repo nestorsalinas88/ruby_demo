2270fe8120c1be0a23f44d34416b73c3ff2e632c
nestor salinas <nestorasm88@gmail.com>
2018-08-01 09:22:12 -0500

    chore: practice with the self keyword

--- employee.rb
+++ employee.rb
@@ -14,15 +14,18 @@ class Employee
     end
     @salary = salary
   end
-  def initialize
-    @name = "Anon"
-    @salary = 0.0
+  def initialize(name = "anon", salary = 0.0)
+    self.name = name
+    self.salary = salary
   end
   def print_stub
     # inspecting for NIL value
-    p @salary, @name
+    # p @salary, @name
+
+
+
     puts "Name: #{@name}"
-    # LOOK RIGHT HEE
+
     # when diving make sure to use at least one FLOAT
     pay_period = (@salary / 365.0) * 14
     # format sequence to remove extra decimal places
@@ -31,18 +34,54 @@ class Employee
   end
 end
 
-dorian = Employee.new
-dorian.name = "Dorian Sanchez"
-dorian.salary = 41450
+# parameters in initializing method
+dorian = Employee.new("this guy", 40000)
 dorian.print_stub
 
 
-
-sandra = Employee.new
-sandra.name = "Sandra Tolumerke"
-sandra.salary = 100000
+sandra = Employee.new("cole brown", 40000)
 sandra.print_stub
 
 
-empty = Employee.new
+empty = Employee.new("amy",50000)
 empty.print_stub
+
+this = Employee.new("This other dude", 34000.00)
+this.print_stub
+
+class Car
+  def initialize(engine)
+    @engine = engine
+  end
+  def rev_engine
+    @engine.make_sound
+  end
+end
+
+class Engine
+  def initialize(sound = "VrooM!!")
+    @sound = sound
+  end
+
+  def make_sound
+    puts @sound
+  end
+end
+
+#
+# enigne = Engine.new
+# car = Car.new(enigne)
+# car.rev_engine
+
+
+# self keyword
+
+# class MyClass
+#   def first_method
+#     puts "current instance method #{self}"
+#   end
+# end
+#
+# object = MyClass.new
+# puts "my object #{object}"
+# object.first_method
