b5daa71c553b9b186e7b7af32a462c7885911d59
nestor salinas <nestorasm88@gmail.com>
2018-08-02 14:31:51 -0500

    chore: create module mixins

--- clip.rb
+++ clip.rb
@@ -11,6 +11,13 @@ class Clip
   end
 end
 
+module FirstModule
+  def show
+    puts "this is displaying now!"
+  end
+end
+
+
 class Video < Clip
   attr_accessor :resolution
 end
@@ -18,6 +25,15 @@ end
 class Song < Clip
   attr_accessor :beats_per_min
 end
+
+class Photo < Clip
+ include FirstModule
+  puts "this is a photo"
+end
+
+clip = Photo.new
+clip.show
+clip.add_comment("this is awesome! ")
 
 
 video = Video.new
@@ -28,4 +44,29 @@ song = Song.new
 song.add_comment("awesome lyrics dude! ")
 
 
-p video.comments, song.comments
+p video.comments, song.comments, clip.comments
+
+
+# different exercise practicing modules
+
+module Curious
+  def investigate(thing)
+    puts "look at this #{thing}"
+  end
+end
+
+module Clumsy
+  def break(thing)
+    puts "knocks over #{thing}"
+  end
+end
+
+class Monkey
+  include Curious
+  include Clumsy
+end
+
+
+bub = Monkey.new
+bub.investigate('vase')
+bub.break('vase')
