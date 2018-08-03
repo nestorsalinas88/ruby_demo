a717bddc2996ae6e9111ca3d92bf065c8850a4cd
nestor salinas <nestorasm88@gmail.com>
2018-08-03 18:56:09 -0500

    practice with nested ternary operators

---
+++ ternerary.rb
@@ -0,0 +1,15 @@
+
+# ternary operators are used in place of if/else statements
+
+val = rand(1..2)
+
+puts val == 1 ? "this is one" :
+   "this is two "
+
+nestor = rand(1..40)
+
+puts nestor >= 20 ? "this is larger than 20" : "this is lower than 19"
+
+puts nestor
+
+
