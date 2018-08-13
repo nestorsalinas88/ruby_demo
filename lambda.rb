8993253c45f952bca39c9142bdffbbbd9af36cd5
nestor salinas <nestorasm88@gmail.com>
2018-08-13 14:09:33 -0500

    more examples of blocks, lambdas, and procs

--- lambda.rb
+++ lambda.rb
@@ -39,3 +39,50 @@ symbols = my_array.select(&symbol_filter)
 my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
 
 puts symbols
+
+
+# Differences between blocks, lambdas, and Procs
+
+
+=begin
+A block is just a bit of code between do..end or {}. It's not an object on its own, but it can be passed to methods like .each or .select.
+A proc is a saved block we can use over and over.
+A lambda is just like a proc, only it cares about the number of arguments it gets and it returns to its calling method rather than returning immediately.
+
+
+=end
+
+# one more example of parsing a proc to a method
+
+ages = [23, 101, 7, 104, 11, 94, 100, 121, 101, 70, 44]
+
+# Add your code below!
+under_100 = Proc.new { |x| x < 100 }
+
+youngsters = ages.select(&under_100)
+
+puts youngsters
+
+
+# passing a lambda into my method
+
+# hash describe with keyvalue pairs
+crew = {
+    captain: "Picard",
+    first_officer: "Riker",
+    lt_cdr: "Data",
+    lt: "Worf",
+    ensign: "Ro",
+    counselor: "Troi",
+    chief_engineer: "LaForge",
+    doctor: "Crusher"
+}
+
+# lambda being created
+first_half = lambda { |x,y| y < "M"}
+
+# lambda passed into method
+a_to_m = crew.select(&first_half)
+
+#  printing the result
+puts a_to_m
